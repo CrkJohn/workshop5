711f4df772f199219d47de1c77c16fc557d4bb4f
Camilo <jcamilovelandiab@gmail.com>
2019-02-14 18:52:32 -0500

    Parte I
    Co-authored-by: John <JhonhRge@gmail.com>

---
+++ Spring/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.stereotype.Component;
+
+
+@Component("englishSpellChecker")
+public class EnglishSpellChecker implements SpellChecker {
+
+    @Override
+    public String checkSpell(String text) {		
+            return "Checked with english checker:"+text;
+    }
+}
---
+++ Spring/src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -0,0 +1,50 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrammarChecker {
+    
+    @Autowired
+    @Qualifier("spanishSpellChecker")
+    SpellChecker sc;
+    String x;
+
+    public SpellChecker getSpellChecker() {
+        return sc;
+    }
+
+    public void setSpellChecker(SpellChecker sc) {
+        this.sc = sc;
+    }
+
+
+    public String check(String text){
+        
+        StringBuffer sb=new StringBuffer();
+        sb.append("Spell checking output:"+sc.checkSpell(text));
+        sb.append("Plagiarism checking output: Not available yet");
+        
+        return sb.toString();
+    }
+	
+
+    public SpellChecker getSc() {
+        return sc;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setSc(SpellChecker sc) {
+        this.sc = sc;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+    
+}
